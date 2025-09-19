6a74a8a1022235ea65d72d8f4f671677ace09a3f
ayush-yadaav <yadavayush2091a@gmail.com>
2025-09-19 19:35:45 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+// it is complex 
+const asyncHandler = (reqHandler) => {
+    (req, res, next) =>{
+        Promise.resolve(reqHandler(req, res, next))
+        .catch((err)=> next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+// const asyncHandler = ()=> {}
+// const asyncHandler = (func)=> () => {}
+// const asyncHandler = (func)=> async () => {}
+
+// for learning || we can also used this 
+
+// const asyncHandler = (fn)=> async (req,res, next) => { // it is call higher function 
+// try{
+//     await fn(req ,res, next)
+
+// }catch(err){
+//     res.status(err.code || 500).json({
+//         sucess: false,
+//         message: err.message
+//     })  
+// }
+// }
